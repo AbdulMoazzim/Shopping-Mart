28fb6bf250c591cd6eaabb51f08b4f38cd9a2db1
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-06-26 06:43:25 -0700

    Just cleaned up some code and made a common.js file for header so that it does not repeat for every message

---
+++ common.js
@@ -0,0 +1,17 @@
+let header = `<header>
+        <div class="Logo">
+            <img src="FASCO.png" alt="Logo">
+        </div>
+        <div class="nav">
+            <nav>
+                <ul>
+                    <li><a href>Home </a></li>
+                    <li><a href>Deals</a></li>
+                    <li><a href>New&nbsp;Arrivals</a></li>
+                    <li><a href>Packages </a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>`
+
+document.body.insertAdjacentHTML('afterbegin',header)
