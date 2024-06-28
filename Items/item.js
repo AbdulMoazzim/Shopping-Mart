f7b1737cf78cf7b7c274c0f76a27da9a95f8d070
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-06-28 07:11:54 -0700

    update pop up menu

--- Items/item.js
+++ Items/item.js
@@ -1,18 +1,5 @@
 let pant = document.querySelector('.pants')
 let pantType = document.querySelector('.list')
-console.log(pant)
 pant.addEventListener('click', ()=> {
-    console.log("hi");
-    if (pant.classList.contains('pants')) {
-        pantType.style.width = "0px";
-        pantType.style.display = "none";
-        pant.classList.remove('pants')
-        console.log("hello");
-    }
-    else {
-        pantType.style.width = "200px";
-        pantType.style.display = "block";
-        pant.classList.add('pants')
-        console.log("hello");
-    }
+    pantType.classList.toggle('open')
 })
