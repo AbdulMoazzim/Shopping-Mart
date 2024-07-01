a93d2f0ced352e091d98cec22c13e94b92a77482
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-07-01 06:32:12 -0700

    improving raw css in homepage

--- common.js
+++ common.js
@@ -27,7 +27,7 @@ let header = `<header>
                 </ul>
             </nav>
         </div>
-        <div class="header_border"></div>
+        <div id="header_border"></div>
     </div>
     `
 let googleMenu = `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />`
